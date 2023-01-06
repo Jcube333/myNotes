71eb35e714a213734b2acaaeb044428b90954524
Jaimin <Jaiminjagdish@gmail.com>
2023-01-06 18:46:28 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+import fs from "fs";
+import { getNotes } from "./notes.js";
+
+console.log(chalk.green.bold.bgBlue("Hey Jcube333"));
+
+// notes();
+// // fs.writeFileSync("test.txt", "Hey This is Jcube333.");
+// fs.appendFileSync("test.txt", "Building the first Node app.");
---
+++ notes.js
@@ -0,0 +1,3 @@
+export function getNotes() {
+  console.log("Your Notes");
+}

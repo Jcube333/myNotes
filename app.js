c3b972e09ac8007f177929f8af26d429dc630b80
Jaimin <Jaiminjagdish@gmail.com>
2023-01-10 00:19:53 +0530

    Options for command added

--- app.js
+++ app.js
@@ -4,9 +4,25 @@ import { hideBin } from "yargs/helpers";
 const y = yargs(hideBin(process.argv));
 
 // y.version("1.1.0");
-y.command("add", "To Add a new note", () => {
-  console.log("Add");
-});
+y.command(
+  "add",
+  "To Add a new note",
+  {
+    title: {
+      desc: "Add title of your note",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      desc: "Add body of your note",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  (argv) => {
+    console.log(argv.title, argv.body);
+  }
+);
 
 y.command("remove", "To remove an existing note", () => {
   console.log("Rmv");
